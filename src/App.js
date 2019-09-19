f60a8aa958fb6def21eab13e11bde31c4d5db391
Sane <tretyakvl@gmail.com>
2019-09-19 15:48:21 +0300

    Finishes progress rings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import Progress from './components/Progress'
 import './App.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      outerProgress: 0,
+      innerProgress: 0
+    }
+  }
+
+  progressOuter (progress) {
+    this.setState({
+      outerProgress: this.state.outerProgress + progress
+    })
+  }
+
   render () {
     return (
       <div className='clock'>
-        <div />
+        <Progress
+          outerProgress={this.state.outerProgress}
+          innerProgress={this.state.innerProgress}
+        />
+        <div>
+          <button onClick={() => this.progressOuter(5)}>inc</button>
+          <button>dec</button>
+        </div>
       </div>
     )
   }
---
+++ src/components/Progress.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import './Progress.css'
+
+class Progress extends Component {
+  render () {
+    const outerCircumference = 691
+    const innerCircumference = 490
+    const { innerProgress, outerProgress } = this.props
+    const outerOffset = getOffset(outerCircumference, outerProgress)
+    const innerOffset = getOffset(innerCircumference, innerProgress)
+
+    return (
+      <svg
+        className='Progress'
+        viewBox='0 0 250 250'
+        width='250'
+      >
+        <circle
+          className='Progress__outer'
+          r='110'
+          cx='125'
+          cy='125'
+          fill='transparent'
+          stroke='#74E900'
+          strokeWidth='30'
+          strokeLinecap='round'
+          strokeDasharray={outerCircumference}
+          strokeDashoffset={outerOffset}
+        />
+        <circle
+          className='Progress__inner'
+          r='78'
+          cx='125'
+          cy='125'
+          fill='transparent'
+          stroke='#F6511D'
+          strokeWidth='30'
+          strokeLinecap='round'
+          strokeDasharray={innerCircumference}
+          strokeDashoffset={innerOffset}
+        />
+      </svg>
+    )
+  }
+}
+
+function getOffset (circumference, progress) {
+  return -circumference * progress / 100
+}
+
+export default Progress
