fd517a50cbff72a30368b1cd134260eed5a3b0c8
Sane <tretyakvl@gmail.com>
2019-09-25 20:38:25 +0300

    Cleans up after the last merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import StartButton from './components/StartButton'
 import Alert from './components/Alert'
+import Progress from './components/Progress'
+import Display from './components/Display'
+import DurationControls from './components/DurationControls'
 import './App.css'
-import Clock from './components/Clock'
 
 const defaultState = {
   session: 25,
   break: 5,
   secondsPassed: 0,
   isBreak: false,
-<<<<<<< HEAD
   controlsShown: null
-=======
-  controlsShown: false
->>>>>>> b8e39189fa50525c018bcb38fa394c78e27279b9
 }
 
 class App extends Component {
@@ -28,15 +26,9 @@ class App extends Component {
     this.handleStartButton = this.handleStartButton.bind(this)
   }
 
-<<<<<<< HEAD
   showControls (type) {
     this.setState({
       controlsShown: type
-=======
-  showControls () {
-    this.setState({
-      controlsShown: true
->>>>>>> b8e39189fa50525c018bcb38fa394c78e27279b9
     })
   }
 
@@ -53,7 +45,6 @@ class App extends Component {
     return (
       <div className='App'>
         <Alert ref={this.AlertRef} />
-<<<<<<< HEAD
         <div className='App__clock'>
           <Progress
             progress={progress}
@@ -71,16 +62,6 @@ class App extends Component {
             onClick={this.setDuration}
           />}
         </div>
-=======
-        <Clock
-          progress={progress}
-          isBreak={isBreak}
-          timeLeft={timeLeft}
-          handleResetButton={this.handleResetButton}
-          showControls={this.showControls}
-          controlsShown={controlsShown}
-        />
->>>>>>> b8e39189fa50525c018bcb38fa394c78e27279b9
         <div className='App__controls'>
           <StartButton
             onClick={this.handleStartButton}
