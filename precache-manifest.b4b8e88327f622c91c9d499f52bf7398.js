226ca65c233583640dc3c1dd6ae29cbcfdb65433
Sane <tretyakvl@gmail.com>
2019-09-28 20:37:00 +0300

    Updates

---
+++ precache-manifest.b4b8e88327f622c91c9d499f52bf7398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae17719d248d53638fdb5155654ec43",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "3ac71f4dafb0144fd0a3",
+    "url": "/pomodoro-clock/static/css/main.a329f732.chunk.css"
+  },
+  {
+    "revision": "a4a42e35861ab208bbc2",
+    "url": "/pomodoro-clock/static/js/2.9f103ea1.chunk.js"
+  },
+  {
+    "revision": "3ac71f4dafb0144fd0a3",
+    "url": "/pomodoro-clock/static/js/main.d3c21b72.chunk.js"
+  },
+  {
+    "revision": "cc5296a9878b6c335676",
+    "url": "/pomodoro-clock/static/js/runtime~main.f57a18ec.js"
+  },
+  {
+    "revision": "ec5aa5ba3051aac901a9773c8f7cedb3",
+    "url": "/pomodoro-clock/static/media/Alert.ec5aa5ba.wav"
+  }
+]);
